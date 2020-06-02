8b88e24f9ff3af5bfe53cb443dd73339daa11965
Nitish Kumar <nitishkumargenius@gmail.com>
2020-06-03 00:15:05 +0530

    Initial commit

---
+++ app/src/main/java/nitish/example/lyricsfinder/MainActivity.java
@@ -0,0 +1,81 @@
+package nitish.example.lyricsfinder;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    //declaring the variables
+    EditText edtArtistName, edtSongName;
+    Button btnGetLyrics;
+    TextView txtLyrics;
+
+
+//https://api.lyrics.ovh/v1/Rihanna/Diamonds#
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //initializing the variables
+        edtArtistName = findViewById(R.id.edtArtistName);
+        edtSongName = findViewById(R.id.edtSongName);
+        btnGetLyrics = findViewById(R.id.btnGetLyrics);
+        txtLyrics = findViewById(R.id.txtLyrics);
+
+        btnGetLyrics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Toast.makeText(getApplicationContext(),
+                //        "This button is tapped", Toast.LENGTH_SHORT).show();
+
+                //getting the api url
+                String url = "https://api.lyrics.ovh/v1/" + edtArtistName.getText().toString() + "/" +
+                        edtSongName.getText().toString();
+                url.replace(" ", "20%");
+
+                //targeting the api
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try{
+                            txtLyrics.setText(response.getString("lyrics"));
+                        }
+                        catch (JSONException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+
+                requestQueue.add(jsonObjectRequest);
+            }
+        });
+    }
+}
